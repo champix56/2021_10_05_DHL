fafb923041fd9a855e2b8500a5d9706cc5f574d3
alex <desorbaix@free.fr>
2021-10-06 02:00:52 +0200

    addEvent on Note

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-// document.getElementById('jsLoaded').remove();
-document.getElementById('jsLoaded').style.backgroundColor="skyblue";
-document.getElementById('jsLoaded').innerHTML="Js bien chargé";
+function init() {
+  document.getElementById("jsLoaded").remove();
+//   document.getElementById("jsLoaded").style.backgroundColor = "skyblue";
+//   document.getElementById("jsLoaded").innerHTML = "Js bien chargé";
+}
+init();
+
+function addNoteListeners(note){
+    note.querySelector('img.note-toggle').addEventListener('click',function(evt){
+        var content=note.querySelector('.note-content');
+        content.style.display=content.style.display==='none'?'flex':'none';
+    });
+    note.querySelector('img.note-close').addEventListener('click',function(evt){
+        note.remove()
+    });
+}
+addNoteListeners(document.querySelector('#note-0'))
