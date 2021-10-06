cc49e3c31452ba13df7c9a0706dfcaa312f90297
alex <desorbaix@free.fr>
2021-10-06 14:49:17 +0200

    modd interne de l'objet

--- js/notes.js
+++ js/notes.js
@@ -9,7 +9,7 @@
  * @param {string} priority priorité de la note
  */
 function Note(exp,dest,desc,dateCrea,dateExe,titre,priority){
-    var _exp=exp,_dest=dest,_desc=desc,_dateCrea=dateCrea,_dateExe=dateExe,_titre=titre,_priority=priority;
+    var _exp=exp,_desc=desc,_dateCrea=dateCrea,_dateExe=dateExe,_titre=titre,_priority=priority;
     /**
      * maj du titre de la note
      * @param {string} titre 
@@ -31,9 +31,13 @@ function Note(exp,dest,desc,dateCrea,dateExe,titre,priority){
     this.setExpediteur=function(exp){
         _exp=exp;
     };
-    
+    //moddif d'un champs public depuis l'interrieurde l'instance de l'objet
+    this.AddCiviliteToDestinataire=function(civ){
+
+        this.destinataire=civ+' '+this.destinataire;
+    }
     //moddif. et lecture depuis l'exterieur controle
-    this.destinataire=_dest;
+    this.destinataire=dest;
     
     this.toJson=function(){
         return '{"expediteur":"'+_exp+'", "dest":"'+_dest+'", "desc":"'+_desc+'", "titre":"'+_titre+'", "priority":"'+_priority+'"}';
